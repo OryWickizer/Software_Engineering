120a98daa69071c4753fbdb0f8980977ef55fa62
AtharvaW195 <atharvawaingankar195@gmail.com>
2025-11-01 16:20:43 -0400

    Updated customer component and added the order status feature

--- proj2/Ecobites/client/src/App.jsx
+++ proj2/Ecobites/client/src/App.jsx
@@ -9,6 +9,7 @@ import Drivers from './drivers/Drivers'
 import { Routes, Route } from 'react-router-dom';
 import Customer from './customers/Customer';
 import Checkout from './customers/Checkout';
+import OrderStatus from './customers/OrderStatus';
 import Restaurant from './restaurants/Restaurants';
 import MenuItems from './restaurants/MenuItems';
 import CustomerOrders from './restaurants/CustomerOrders';
@@ -23,7 +24,8 @@ function App() {
                  <Route path="/profile" element={<Profile />} />
                  <Route path="/driver" element={<Drivers />} />
                   <Route path="/customer" element={<Customer />} />
-                  <Route path="/checkout" element={<Checkout />} />
+                  <Route path="/customer/checkout" element={<Checkout />} />
+                  <Route path="/customer/orders" element={<OrderStatus />} />
                   <Route path="/restaurants" element={<Restaurant />} />
                    <Route path="/restaurants/menu" element={<MenuItems />} />
                    <Route path="/restaurants/orders" element={<CustomerOrders />} />
