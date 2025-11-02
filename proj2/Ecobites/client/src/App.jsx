e79d35fd15bf51ce727802e8bd137049690de608
yashdive <yashdive190@gmail.com>
2025-11-01 21:13:20 -0400

    created protected and role based routes

--- proj2/Ecobites/client/src/App.jsx
+++ proj2/Ecobites/client/src/App.jsx
@@ -15,6 +15,8 @@ import MenuItems from './restaurants/MenuItems';
 import CustomerOrders from './restaurants/CustomerOrders';
 import About from './pages/About';
 import { RestaurantProvider } from './context/RestaurantContext';
+import {ProtectedRoute} from './routes/ProtectedRoute';
+import { RoleBasedRoute } from './routes/RoleBasedRoute';
 
 function App() {
   return (
@@ -22,19 +24,36 @@ function App() {
              <div className="flex min-h-screen flex-col">
                <SiteHeader />
                <main className="flex-1">
-               <Routes>
-                 <Route path="/" element={<Index />} />
-                 <Route path="/login" element={<Login />} />
-                 <Route path="/profile" element={<Profile />} />
-                 <Route path="/driver" element={<Drivers />} />
-                  <Route path="/customer" element={<Customer />} />
-                  <Route path="/customer/checkout" element={<Checkout />} />
-                  <Route path="/customer/orders" element={<OrderStatus />} />
-                  <Route path="/restaurants" element={<Restaurant />} />
-                   <Route path="/restaurants/menu" element={<MenuItems />} />
-                   <Route path="/restaurants/orders" element={<CustomerOrders />} />
-                 <Route path="/about" element={<About />} />
-               </Routes>
+                <Routes>
+      {/* Public */}
+      <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<About />} />
+
+      {/* Protected */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/profile" element={<Profile />} />
+
+        {/* Customer Routes */}
+        <Route element={<RoleBasedRoute allowedRoles={['customer']} />}>
+          <Route path="/customer" element={<Customer />} />
+          <Route path="/customer/checkout" element={<Checkout />} />
+          <Route path="/customer/orders" element={<OrderStatus />} />
+        </Route>
+
+        {/* Restaurant Routes */}
+        <Route element={<RoleBasedRoute allowedRoles={['restaurant']} />}>
+          <Route path="/restaurants" element={<Restaurant />} />
+          <Route path="/restaurants/menu" element={<MenuItems />} />
+          <Route path="/restaurants/orders" element={<CustomerOrders />} />
+        </Route>
+
+        {/* Driver Routes */}
+        <Route element={<RoleBasedRoute allowedRoles={['driver']} />}>
+          <Route path="/driver" element={<Drivers />} />
+        </Route>
+      </Route>
+    </Routes>
              </main>
              <SiteFooter />
            </div>
--- proj2/Ecobites/client/src/routes/RoleBasedRoute.jsx
+++ proj2/Ecobites/client/src/routes/RoleBasedRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../context/AuthContext';
 
 export const RoleBasedRoute = ({ children, allowedRoles }) => {
@@ -7,10 +7,13 @@ export const RoleBasedRoute = ({ children, allowedRoles }) => {
   if (loading) {
     return <div>Loading...</div>;
   }
+   if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
   if (!user || !allowedRoles.includes(user.role)) {
     return <Navigate to="/" replace />;
   }
 
-  return children;
+  return <Outlet />;
 };
